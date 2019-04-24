da4ddcb4b5a37d198008ea4898db31258a96bfce
Sagar Gujarati <sagar199785@gmail.com>
2019-04-24 19:19:32 +0530

    server deployed

--- server/App.js
+++ server/App.js
@@ -61,4 +61,4 @@ app.use((err, req, res) => {
   });
 });
 
-const server = app.listen(8000, () => console.log('Server started on http://localhost:8000'));
+const server = app.listen(process.env.PORT || 8000, () => console.log('Server started on http://localhost:8000'));
